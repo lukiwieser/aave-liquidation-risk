ca3afd1fe1ef9075f6f957d9ec4904ebab0c9d1a
Sebastian Fürndraht <sebastian.fuerndraht@gmail.com>
2021-05-31 12:40:12 +0200

    added basic api structure to download-lendings

--- scripts/download-lendings.py
+++ scripts/download-lendings.py
@@ -1,15 +1,27 @@
 from argparse import ArgumentParser
+from urllib import parse
 import json
+import requests
 
+BASE_URL = 'https://api.etherscan.io/api'
+API_KEY = 'S7XRYA2VNPPU4ZWPNWGSJT2G7F33NSTNP7'
+ADDRESS = '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9'
+
+def get_address_info():
+    url = BASE_URL + '?' + parse.urlencode({'module' : 'account', 'action' : 'txlist', 'address' : ADDRESS, 'sort' : 'asc', 'startblock' : 0, 'endblock' : 999999999, 'apikey' : API_KEY})
+    r = requests.get(url)
+    d = json.loads(r.text)
+  
 def main(args):
-    
+
     output_file = args.output
 
     results = dict()
 
     # write out the results of Q1 and Q2 to JSON file
-    with open(output_file, 'w') as fp:
-        json.dump(results, fp, indent=4)
+    # with open(output_file, 'w') as fp:
+    #   json.dump(results, fp, indent=4)
+    get_address_info()
 
 
 if __name__ == '__main__':
@@ -17,7 +29,7 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-o', '--output',
                         help='Output file path (JSON)',
-                        type=str, required=True)
+                        type=str, required=False)
     args = parser.parse_args()
 
     main(args)
