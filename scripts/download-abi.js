027f6ee8a918fc4c3c7d8333a1f70407712a9f00
Lukas Wieser <lukas.luki.wieser@gmail.com>
2021-06-01 23:07:40 +0200

    adjusted script for downloading abi to be more consistent with folder-structure

--- scripts/download-abi.js
+++ scripts/download-abi.js
@@ -1,7 +1,4 @@
 const LendingPoolV2Artifact = require('@aave/protocol-v2/artifacts/contracts/protocol/lendingpool/LendingPool.sol/LendingPool.json');
 const fs = require('fs')
 
-// Log the ABI into console
-//console.log(LendingPoolV2Artifact.abi)
-
-fs.writeFileSync("out.json", LendingPoolV2Artifact.abi)
+fs.writeFileSync("../data/raw/abi.json", JSON.stringify(LendingPoolV2Artifact.abi))
