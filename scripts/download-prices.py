8bf567233bf06c29900e4d9dfbfebfb678dbbeb2
Lukas Wieser <lukas.luki.wieser@gmail.com>
2021-06-25 11:13:13 +0200

    removed unused imports and unneccessary comments

--- scripts/download-prices.py
+++ scripts/download-prices.py
@@ -1,28 +1,9 @@
-from urllib.parse import quote
-from bs4 import BeautifulSoup
 import pandas as pd
 import json
-import time
-from argparse import ArgumentParser
-import codecs
 import requests
 from urllib import parse
 from pathlib import Path
 
-#def main(args):
-#    output_file = args.output#
-#
-#    url = "https://coinmarketcap.com/de/currencies/ethereum/historical-data"
-#
-#
-#    cmc = requests.get(url)
-#    soup = BeautifulSoup(cmc.content, 'html.parser')
-#    print(soup.title)
-#    html = soup.prettify()
-#
-#    file = codecs.open("lol", "w", "utf-8")
-#    file.write(html)
-#    file.close()
 
 BASE_URL = "https://rest.coinapi.io/v1"
 API_KEY = "A098ECCE-FC1E-4594-9C68-9ADF339D40AD"
@@ -33,10 +14,8 @@ def save_dataframe(df, out_directory, out_file):
     out_directory.mkdir(parents=True, exist_ok=True)
     df.to_csv(out_directory / out_file, index = False, header=True)
 
-def main(args):
-    output_file = args.output#
-    #https://rest.coinapi.io/v1/ohlcv/BTC/USD/history?period_id=1DAY&time_start=2020-12-01T00:00:00&apikey=A098ECCE-FC1E-4594-9C68-9ADF339D40AD&limit=200
 
+def main():
     asset = "BTC"
     quote = "USD"
     queryParams = parse.urlencode({
@@ -56,15 +35,7 @@ def main(args):
     df = pd.DataFrame(data, columns=['date', 'open', 'high', 'low', 'close'])
 
     save_dataframe(df, "../data/raw/price-history", asset + "-" + quote + ".csv")
-    #df.to_csv("../data/raw/price-history-" + asset +".csv", index = False, header=True)
 
     
 if __name__ == '__main__':
-
-    parser = ArgumentParser()
-    parser.add_argument('-o', '--output',
-                        help='Output file path (JSON)',
-                        type=str, required=False)
-    args = parser.parse_args()
-
-    main(args)
+    main()
