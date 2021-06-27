9cd1db07834ce8f83c96e1125667bdffafb5b7f2
Lukas Wieser <lukas.luki.wieser@gmail.com>
2021-06-28 01:10:39 +0200

    adjusted evaluation to collect some user-data

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,10 +1,49 @@
 from argparse import ArgumentParser
 import json
 import pandas as pd
+from enum import Enum
 
 def load_tx_history(file):
     df = pd.read_csv(file, sep=";") 
     return df
+
+
+class UserData:
+    def __init__(self):
+        self.collateral_timeline = dict()
+        self.debt_timeline = dict()
+    
+    def add_deposit(self, asset, amount, timestamp):
+        if asset not in self.collateral_timeline:
+            self.collateral_timeline[asset] = []
+        self.collateral_timeline[asset].append(["deposit", amount, str(timestamp)])
+
+    def add_withdraw(self, asset, amount, timestamp):
+        if asset not in self.collateral_timeline:
+            self.collateral_timeline[asset] = []
+        self.collateral_timeline[asset].append(["withdraw", amount, str(timestamp)])
+
+    def add_borrow(self, asset, amount, timestamp):
+        if asset not in self.debt_timeline:
+            self.debt_timeline[asset] = []
+        self.debt_timeline[asset].append(["borrow", amount, str(timestamp)])
+
+    def add_repay(self, asset, amount, timestamp):
+        if asset not in self.debt_timeline:
+            self.debt_timeline[asset] = []
+        self.debt_timeline[asset].append(["repay", amount, str(timestamp)])
+
+    def add_liquidation_victim(self, collateral_asset, debt_asset, debt_to_cover, timestamp):
+        if debt_asset not in self.debt_timeline:
+            self.debt_timeline[debt_asset] = []
+        self.debt_timeline[debt_asset].append(["liquidation", debt_to_cover, str(timestamp)])
+
+    def to_json(self):
+        data = dict()
+        data["collateral_timeline"] = self.collateral_timeline
+        data["debt_timeline"] = self.debt_timeline
+        return json.dumps(data)
+
 
 def main(args):
     txhistory_file = args.txhistory
@@ -41,11 +80,7 @@ def main(args):
     liqidation_addresses_debt = dict()
     liqidation_addresses_collateral = dict()
     liqidation_addresses_pair = dict()
-
-    # all
-    # collateral & dept
-    # pairs
-
+    user_data = dict()
 
     for index, row in tx_history.iterrows():
         input_decoded = json.loads(row['input_decoded'])
@@ -80,19 +115,74 @@ def main(args):
             else:
                 liqidation_addresses_pair[pair] = 1
 
-    liqidation_rate_overall = tx_method_types["liquidationCall"] / tx_method_types["borrow"]
+        # users
+        user = row["from"]
+        if user not in user_data:
+            user_data[user] = UserData()
+        
+        if input_decoded["method"] == "deposit":
+            asset_address = input_decoded["inputs"][0]
+            asset = assetAddresses.get(asset_address, asset_address) 
+            amount = int(input_decoded["inputs"][1],16)
+            timestamp = row["timestamp"]
+            user_data[user].add_deposit(asset, amount, timestamp)
+
+        if input_decoded["method"] == "withdraw":
+            asset_address = input_decoded["inputs"][0]
+            asset = assetAddresses.get(asset_address, asset_address)  #change ???
+            amount = int(input_decoded["inputs"][1],16)
+            timestamp = row["timestamp"]
+            user_data[user].add_withdraw(asset, amount, timestamp)
+
+        if input_decoded["method"] == "borrow":
+            asset_address = input_decoded["inputs"][0]
+            asset = assetAddresses.get(asset_address, asset_address) 
+            amount = int(input_decoded["inputs"][1],16)
+            timestamp = row["timestamp"]
+            user_data[user].add_borrow(asset, amount, timestamp)
+
+        if input_decoded["method"] == "repay":
+            asset_address = input_decoded["inputs"][0]
+            asset = assetAddresses.get(asset_address, asset_address) 
+            amount = int(input_decoded["inputs"][1],16)
+            timestamp = row["timestamp"]
+            user_data[user].add_repay(asset, amount, timestamp)
+
+        if input_decoded["method"] == "liquidationCall":
+            collateral_asset_address = input_decoded["inputs"][0]
+            debt_asset_address = input_decoded["inputs"][1]
+            liquidation_victim = input_decoded["inputs"][2]
+            collateral_asset = assetAddresses.get(collateral_asset_address, collateral_asset_address) 
+            debt_asset = assetAddresses.get(debt_asset_address, debt_asset_address) 
+            debt_to_cover = int(input_decoded["inputs"][3],16)
+            timestamp = row["timestamp"]
+
+            if liquidation_victim not in user_data:
+                user_data[liquidation_victim] = UserData()
+            user_data[liquidation_victim].add_liquidation_victim(collateral_asset, debt_asset, debt_to_cover, timestamp)
+
+
+
+    liqidation_rate_overall = tx_method_types.get("liquidationCall",0) / tx_method_types.get("borrow",0)
 
     results['liqidation_rate_overall'] = liqidation_rate_overall
     results['tx_method_types'] = tx_method_types
     results['liqidation_addresses_debt'] = liqidation_addresses_debt
     results['liqidation_addresses_collateral'] = liqidation_addresses_collateral
-    results['liqidation_addresses_pair'] = {k: v for k, v in sorted(liqidation_addresses_pair.items(), key=lambda item: item[1], reverse=True)}
+    results['liqidation_addresses_pair (collateral / debt)'] = {k: v for k, v in sorted(liqidation_addresses_pair.items(), key=lambda item: item[1], reverse=True)}
+    results["tx_count"] = len(tx_history.index)
+    #results['users_count'] = len(users)
 
     with open(output_file, 'w') as fp:
         json.dump(results, fp, indent=4)
 
-if __name__ == '__main__':
+    with open('readme.txt', 'w') as f:
+        f.write("[\n")        
+        for user, userdata in user_data.items():
+            f.write('{"' + user + '":' + userdata.to_json()+"},\n")
+        f.write("]")        
 
+if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-o', '--output',help='Output file path (JSON)', type=str, required=False)
     parser.add_argument('-t', '--txhistory',help='Input file path of parsed aave tx history (CSV)', type=str,required=True)
