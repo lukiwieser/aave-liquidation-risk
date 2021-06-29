e5e9c80430ca98c3097eac88578732020dd8409e
Lukas Wieser <lukas.luki.wieser@gmail.com>
2021-06-29 18:09:50 +0200

    added script to download tx-history of weth-gateway; added abi of weth-gateway; added script to parse tx-history of weth-gateway

---
+++ scripts/parse-tx-history-weth-gateway.js
@@ -0,0 +1,46 @@
+const InputDataDecoder = require('ethereum-input-data-decoder');
+const csv = require('csv-parser');
+const fs = require('fs');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+
+const decoder = new InputDataDecoder("../data/abi-weth-gateway.json");
+
+const data = []
+count = 0;
+
+fs.createReadStream('../data/raw/tx-history-weth-gateway.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    newRow = {...row}
+    const result = decoder.decodeData(row.input);
+    row.input_decoded = JSON.stringify(result);
+    data.push(row)
+    
+    count++
+    if(count % 1000 === 0) console.log(count);
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed');
+
+    const csvWriter = createCsvWriter({
+      path: '../data/parsed/tx-history-weth-gateway.csv',
+      fieldDelimiter: ';',
+      header: [
+        {id: 'timestamp', title: 'timestamp'},
+        {id: 'blockNumber', title: 'blockNumber'},
+        {id: 'hash', title: 'hash'},
+        {id: 'from', title: 'from'},
+        {id: 'to', title: 'to'},
+        {id: 'value', title: 'value'},
+        {id: 'input', title: 'input'},
+        {id: 'input_decoded', title: 'input_decoded'},
+        {id: 'isError', title: 'isError'},
+        {id: 'txreceipt_status', title: 'txreceipt_status'},
+      ]
+    });
+
+    csvWriter.writeRecords(data).then(
+      ()=> console.log('The CSV file was written successfully')
+    );
+  });
